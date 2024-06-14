054fb5c429ce34618eacef6f9471d105ce4e1103
Kevin Morris <kevr@0cost.org>
2024-06-13 19:49:08 -0700

    feat: introduce .clang-format requirement based on llvm
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- src/droid0.cpp
+++ src/droid0.cpp
@@ -3,12 +3,13 @@
  *
  * SPDX-License-Identifier: MIT
  **/
-#include <iostream>
 #include "config.hpp"
+#include <iostream>
 
-int main(int argc, const char **argv) {
-    if(argc != 1) {
-        std::cout << argv[0] <<  " takes no arguments.\n";
+int main(int argc, const char **argv)
+{
+    if (argc != 1) {
+        std::cout << argv[0] << " takes no arguments.\n";
         return 1;
     }
     std::cout << "This is project " << NAME << ", version " << VERSION << ".\n";
