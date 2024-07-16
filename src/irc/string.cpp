812577fe8cebc5bc109387059a7e3e6ac1913433
Kevin Morris <kevr@0cost.org>
2024-07-15 22:12:43 -0700

    feat: added irc::string::* helpers
    Signed-off-by: Kevin Morris <kevr@0cost.org>

---
+++ src/irc/string.cpp
@@ -0,0 +1,36 @@
+/**
+ * IRC string factory functions
+ *
+ * SPDX-License-Identifier: MIT
+ **/
+#include "string.hpp"
+#include <sstream>
+
+namespace droid0::irc::string
+{
+
+std::string registration(const std::string &nick, const std::string &user,
+                         const std::string &realname,
+                         const std::string &hostname)
+{
+    std::stringstream ss;
+    ss << "NICK " << nick << '\n';
+    ss << "USER " << user << ' ' << hostname << " * :" << realname << '\n';
+    return ss.str();
+}
+
+std::string pong(const std::string &arg)
+{
+    std::stringstream ss;
+    ss << "PONG " << arg << '\n';
+    return ss.str();
+}
+
+std::string join(const std::string &channel)
+{
+    std::stringstream ss;
+    ss << "JOIN " << channel << '\n';
+    return ss.str();
+}
+
+}; // namespace droid0::irc::string
---
+++ src/irc/string.hpp
@@ -0,0 +1,23 @@
+/**
+ * IRC string factory functions
+ *
+ * SPDX-License-Identifier: MIT
+ **/
+#ifndef IRC_STRING_HPP
+#define IRC_STRING_HPP
+
+#include <string>
+
+namespace droid0::irc::string
+{
+
+std::string registration(const std::string &nick, const std::string &user,
+                         const std::string &realname,
+                         const std::string &hostname = "*");
+
+std::string pong(const std::string &arg);
+std::string join(const std::string &channel);
+
+}; // namespace droid0::irc::string
+
+#endif /* IRC_STRING_HPP */
